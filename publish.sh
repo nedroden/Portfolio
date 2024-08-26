dcc36c5325b91d33cb32c2f32feb65704761206c
Robert Monden <robert@robertmonden.com>
2024-08-26 18:58:17 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Load configuration
-./environment.sh
-
 # Build projects
 dotnet publish RobertMonden.Website -c Release -o Dist
 for f in Dist/wwwroot/_framework/*; do mv "$f" "`echo $f | sed -e 's/\.dll/.bin/g'`"; done
